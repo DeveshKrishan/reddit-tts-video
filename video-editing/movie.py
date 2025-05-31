3abd82f24cb64ca4cbfe51cd1dc9fd4678ca545d
Devesh Krishan <deveshkrishan2003@gmail.com>
2025-05-31 14:16:23 -0700

    add audio to clip

--- video-editing/movie.py
+++ video-editing/movie.py
@@ -1,12 +1,11 @@
 import os
 
-from moviepy import CompositeVideoClip, VideoFileClip
+from moviepy import AudioFileClip, CompositeVideoClip, VideoFileClip
 
 OUTPUT_FOLDER = "assets/video"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 clip = VideoFileClip("video/input3.mp4")
 
-# Set crop size for YouTube Shorts (9:16, 1080x1920)
 target_width = 1080
 target_height = 1920
 
@@ -15,11 +14,13 @@ center_x, center_y = clip.w // 2, clip.h // 2
 crop_x = center_x - target_width / 2
 crop_y = center_y - target_height / 2
 
-clip = clip.without_audio()
+# clip = clip.without_audio()
 clip = clip.cropped(x1=crop_x, y1=crop_y, width=target_width, height=target_height)
 
 
-# audio = AudioClip("audio/1ehlrdd.mp3")
+audio = AudioFileClip("audio/1ehlrdd.mp3")
+clip = clip.with_audio(audio)
+# clip.with_effects([afx.AudioFadeIn("00:00:06")])
 
 # clip = clip.with_audio(audio)
 final_video = CompositeVideoClip([clip])
